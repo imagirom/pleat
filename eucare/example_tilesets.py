ac7770e969e06cd757a486cb59a2e86248cb29de
Roman Remme <roman.remme@gmx.de>
2019-08-22 16:01:49 +0200

    add platonic, pgg_2x

--- eucare/example_tilesets.py
+++ eucare/example_tilesets.py
@@ -11,10 +11,18 @@ class TileSet:
 
 
 def align_tiles(tile1, label1, tile2, label2):
-    tile1.edge_instructions[label1] = tile2.attatch_instruction(label2)
-    tile2.edge_instructions[label2] = tile1.attatch_instruction(label1)
+    tile1.edge_instructions[label1] = tile2.attach_instruction(label2)
+    tile2.edge_instructions[label2] = tile1.attach_instruction(label1)
 
 # TODO: alternate approach: do something like f12.to_attach = [f4, f6] * 6
+
+
+def platonic(n):
+    assert n in (3, 4, 6)
+    t = RegularEuclideanTile(n, edge_labels=['a'] * n)
+    align_tiles(t, 'a', t, 'a')
+    return [t]
+
 
 def t_4_6_12():
     f4 = RegularEuclideanTile(4, edge_labels=[6, 12] * 2)
@@ -23,13 +31,8 @@ def t_4_6_12():
     align_tiles(f4, 6, f6, 4)
     align_tiles(f6, 12, f12, 6)
     align_tiles(f4, 12, f12, 4)
-    # f4.edge_instructions[6] = f6.attatch_instruction(4)
-    # f4.edge_instructions[12] = f12.attatch_instruction(4)
-    # f6.edge_instructions[4] = f4.attatch_instruction(6)
-    # f6.edge_instructions[12] = f12.attatch_instruction(6)
-    # f12.edge_instructions[4] = f4.attatch_instruction(12)
-    # f12.edge_instructions[6] = f6.attatch_instruction(12)
     return f4, f6, f12
+
 
 def t_3_3_4_3_4():
     tri = RegularEuclideanTile(3)
@@ -39,6 +42,35 @@ def t_3_3_4_3_4():
     align_tiles(tri, 1, sq1, 1)
     align_tiles(tri, 2, sq2, 2)
     return sq1, sq2, tri
+
+# for printing: TA3 (Triangle, A, side 3) or Q1. Letters in ceter, numbers in sides.
+
+
+def pgg_2x():
+    tri_11 = RegularEuclideanTile(3)
+    tri_12 = RegularEuclideanTile(3)
+    tri_21 = RegularEuclideanTile(3)
+    tri_22 = RegularEuclideanTile(3)
+    sq1 = RegularEuclideanTile(4)
+    sq2 = RegularEuclideanTile(4)
+
+    align_tiles(sq1, 0, sq1, 0)
+    align_tiles(sq1, 1, tri_11, 2)
+    align_tiles(sq1, 2, tri_22, 1)
+    align_tiles(sq1, 3, tri_12, 1)
+    align_tiles(tri_11, 0, tri_11, 0)
+    align_tiles(tri_11, 1, tri_12, 0)
+
+    align_tiles(sq2, 0, sq2, 0)
+    align_tiles(sq2, 1, tri_22, 2)
+    align_tiles(sq2, 2, tri_12, 2)
+    align_tiles(sq2, 3, tri_21, 1)
+    align_tiles(tri_21, 0, tri_21, 0)
+    align_tiles(tri_21, 2, tri_22, 0)
+
+    return sq1, sq2, tri_11, tri_12, tri_21, tri_22
+
+
 
 # # define 6.4.3.4 tiling
 # hexagon = RegularEuclideanTile(6, edge_labels=['a', 'a', 'a', 'a', 'a', 'a'])
