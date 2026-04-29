7a0d3013dbfaa1d77465993a9bd9aeb3fba78fa1
Roman Remme <roman.remme@gmx.de>
2026-04-29 14:04:24 +0200

    Refactor conway.py into eucare.conway package (operators + factories)

---
+++ eucare/conway/__init__.py
@@ -0,0 +1,44 @@
+"""Conway topological operators for transforming tilings.
+
+Re-exports the operator classes and factory functions from the submodules.
+"""
+from __future__ import annotations
+
+from .factories import (
+    alternating_flagstone_graph,
+    ambo_graph,
+    chamfer_graph,
+    dual_graph,
+    expand_graph,
+    flagstone_pvitelli_graph,
+    goldberg2_graph,
+    gyro_graph,
+    join_graph,
+    kis_graph,
+    lace_graph,
+    loft_graph,
+    starify_graph,
+    truncate_graph,
+    twist_rotate_graph,
+)
+from .operators import GeometricConwayOperator, TopologicalConwayOperator
+
+__all__ = [
+    "GeometricConwayOperator",
+    "TopologicalConwayOperator",
+    "alternating_flagstone_graph",
+    "ambo_graph",
+    "chamfer_graph",
+    "dual_graph",
+    "expand_graph",
+    "flagstone_pvitelli_graph",
+    "goldberg2_graph",
+    "gyro_graph",
+    "join_graph",
+    "kis_graph",
+    "lace_graph",
+    "loft_graph",
+    "starify_graph",
+    "truncate_graph",
+    "twist_rotate_graph",
+]
